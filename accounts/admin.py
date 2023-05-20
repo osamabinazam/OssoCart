a75bc0058975863b0577438f6bea64196e319164
Osama Bin Azam <osamabinazam@gmail.com>
2023-05-20 12:29:28 +0500

    Made password to read only mode

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models  import Account
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
-admin.site.register(Account)
+
+# Setting password to  readonly mode
+class AccountAdmin (UserAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'username', 'date_joined',  'last_login', 'is_active')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+
+admin.site.register(Account, AccountAdmin )
