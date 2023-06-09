5500472a2694efd3bac823ec0bcd78fe0269b189
Osama Bin Azam <osamabinazam@gmail.com>
2023-06-09 22:26:58 +0500

    Display Dynamic Products

--- ossocart/views.py
+++ ossocart/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from store.models import Product
 
 # Create your views here.
 
 def Home(request):
-    context = {}
+    products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products':products
+    }
     return render(request, 'ossocart/home.html', context)
