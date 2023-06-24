66bf44392fc59f4a4097d6beddb87f3a1a606429
Osama Bin Azam <osamabinazam@gmail.com>
2023-06-24 21:45:29 +0500

    Product by Category

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Store, name='store' ),
+    path('<slug:category_slug>/', views.Store, name='products_by_category' ),
 ]
 
 
--- store/views.py
+++ store/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import Category
 # Create your views here.
 
 # Rendering Store that list all available Products
 
-def Store(request):
-    products = Product.objects.all().filter(is_available=True)
+def Store(request, category_slug=None):
+    categories = None
+    products = None
+    
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+        product_counts = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_counts = products.count()
     context = {
         'products':products,
+        'product_counts':product_counts,
     }
     return render(request, 'ossocart/store.html', context)
